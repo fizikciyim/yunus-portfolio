f2683edcb50841e195b26ca20293391ec104d62b
fizikciyim <yunus.karasenn@gmail.com>
2025-08-01 17:42:28 +0300

    Arka plan rengi değişti

--- src/sablonlar/Header.jsx
+++ src/sablonlar/Header.jsx
@@ -2,7 +2,10 @@ function Header() {
   return (
     <>
       {/* Header/Home */}
-      <div className="w3-container w3-padding-32 w3-center w3-black" id="home">
+      <div
+        className="w3-container w3-padding-32 w3-center w3-black divdiv"
+        id="home"
+      >
         <h1 className="w3-jumbo">Yunus Karaşen</h1>
         <p>Yazılımcı</p>
         <img
